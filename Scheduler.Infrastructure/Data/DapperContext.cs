33e40f4c9cfbef827c52099f6de3227487de09a0
anapbuchholz@gmail.com <anapbuchholz@gmail.com>
2025-07-07 16:17:26 -0300

    [feature] Creating infrastructure folder and dapper context

---
+++ Scheduler.Infrastructure/Data/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Scheduler.Infrastructure.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new NpgsqlConnection(_connectionString);
+    }
+}
