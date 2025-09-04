162737ae0ca555b9cc07b90c0e9e5c5f19c5f779
Tomás Antônio Costa Campos <tomasantoniocampos@gmail.com>
2025-09-03 21:35:01 -0300

    async

--- Scheduler.Application/Infrastructure/Data/PostgreSql/Repositories/User/Repository/UserRepository.cs
+++ Scheduler.Application/Infrastructure/Data/PostgreSql/Repositories/User/Repository/UserRepository.cs
@@ -9,20 +9,20 @@ namespace Scheduler.Application.Infrastructure.Data.PostgreSql.Repositories.User
     {
         private readonly IDataContext _context = context;
 
-        public Task<UserEntity?> GetUserByEmailAsync(string email)
+        public async Task<UserEntity?> GetUserByEmailAsync(string email)
         {
             var query = UserSqlConstants.SELECT_USER_BY_EMAIL;
 
             var connection = _context.GetConnection();
-            return connection.QueryFirstOrDefaultAsync<UserEntity>(query, new { Email = email });
+            return await connection.QueryFirstOrDefaultAsync<UserEntity>(query, new { Email = email });
         }
 
-        public Task RegisterUserAsync(UserEntity user)
+        public async Task RegisterUserAsync(UserEntity user)
         {
             var command = UserSqlConstants.INSERT_USER;
 
             var connection = _context.GetConnection();
-            return connection.ExecuteAsync(command, user);
+            await connection.ExecuteAsync(command, user);
         }
     }
 }
