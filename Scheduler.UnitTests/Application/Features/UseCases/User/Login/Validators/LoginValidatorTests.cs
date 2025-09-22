c17af21c5a16c468c63f2f7543f4ff30e7a62e6e
Tomás Antônio Costa Campos <tomasantoniocampos@gmail.com>
2025-09-22 20:52:45 -0300

    LoginValidatorTests

---
+++ Scheduler.UnitTests/Application/Features/UseCases/User/Login/Validators/LoginValidatorTests.cs
@@ -0,0 +1,125 @@
+﻿using Scheduler.Application.Features.UseCases.User.Login.UseCase;
+using Scheduler.Application.Features.UseCases.User.Login.Validators;
+using System.Threading.Tasks;
+
+namespace Scheduler.UnitTests.Application.Features.UseCases.User.Login.Validators
+{
+    [TestClass]
+    public class LoginValidatorTests
+    {
+        private LoginValidator _validator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _validator = new LoginValidator();
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithValidEmailAndPassword_ShouldBeValid()
+        {
+            var request = new LoginRequest
+            {
+                Email = "user@email.com",
+                Password = "123456"
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsTrue(result.IsValid);
+            Assert.AreEqual(0, result.Errors.Count);
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithEmptyEmail_ShouldReturnEmailError()
+        {
+            var request = new LoginRequest
+            {
+                Email = "",
+                Password = "123456"
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "O E-mail deve ser informado.");
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithNullEmail_ShouldReturnEmailError()
+        {
+            var request = new LoginRequest
+            {
+                Email = null,
+                Password = "123456"
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "O E-mail deve ser informado.");
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithEmptyPassword_ShouldReturnPasswordError()
+        {
+            var request = new LoginRequest
+            {
+                Email = "user@email.com",
+                Password = ""
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "A senha deve ser informada.");
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithNullPassword_ShouldReturnPasswordError()
+        {
+            var request = new LoginRequest
+            {
+                Email = "user@email.com",
+                Password = null
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "A senha deve ser informada.");
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithEmptyEmailAndPassword_ShouldReturnBothErrors()
+        {
+            var request = new LoginRequest
+            {
+                Email = "",
+                Password = ""
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "O E-mail deve ser informado.");
+            CollectionAssert.Contains(result.Errors, "A senha deve ser informada.");
+        }
+
+        [TestMethod]
+        public async Task ValidateAsync_WithNullEmailAndPassword_ShouldReturnBothErrors()
+        {
+            var request = new LoginRequest
+            {
+                Email = null,
+                Password = null
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.Contains(result.Errors, "O E-mail deve ser informado.");
+            CollectionAssert.Contains(result.Errors, "A senha deve ser informada.");
+        }
+    }
+}
